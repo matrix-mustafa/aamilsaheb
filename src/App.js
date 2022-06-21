10ab61d07efab226662a5103366bbd0fb20f9dc4
Your Name <abhishekkumar9608@gmail.com>
2022-06-21 14:10:54 +0530

    created card component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Col from 'react-bootstrap/Col';
 import Badge from 'react-bootstrap/Badge';
 import { useEffect, useState } from 'react';
 import useFetch from "./useFetch";
+import EducationDetail from './components/EducationDetail';
 
 function App() {
 
@@ -15,8 +16,6 @@ const [streamData , setStreamData] = useState(null);
 const [dropoutList , setDropoutList] = useFetch('https://www.talabulilm.com/api2022/profile/aamilsaheb/dropOutUserList/170');
 const [razaData , setRazaData] = useState(null);
 const [quranSanad , setquranSanad] = useState(null);
-const [active , setActive] = useState(0);
-const [streamActive , setStreamActive] = useState(null);
 const [EduStatus , setEduStatus] = useState("Drop Outs");
 
 
@@ -140,75 +139,7 @@ console.log(dropoutList)
 </Col>
 <Col xs={9} style={{marginTop:"20px"}}  >
    <h5>Showing results for "{EduStatus}" in Surat Jamaat</h5>
-  {
-   dropoutList &&  dropoutList.map((item) => (
-  <div className='user-card'>
-    <div className='d-flex'>
-      <div className='d-flex' style={{alignItems:"center" , height:"100%" , width:"50%"}} >
-        <img src={`https://www.talabulilm.com/mumin_images/${item.its_id}.png`} className="user-img"/>
-        <div className='user-details'>
-          <p>{item.its_id}</p>
-          <p>{item.name}</p>
-          <p>{item.gender == 'M' ? 'Male' : 'Female'} {item.age} years</p>
-          <p>{item.email}</p>
-          <p>{item.mobile}</p>
-        </div>
-      </div>
-
-   <div style={{width:"50%"}} >
-    {item.future_edu_course !== " " &&
-    <div className='d-flex user-courses-container future'>
-      <div style={{fontWeight:700 , fontSize:"16px" , lineHeight:"19px" , fontFamily:"Inter"}}>Future Education</div>
-      <div>
-        <div className='course-text'>
-          {item.future_edu_course}
-        </div>
-        <div className='course-text'>
-          {item.future_edu_institute}
-        </div>
-        <div className={`jawab-button ${item.future_edu_jawab === "" ? "danger-btn" : "success-btn"}`}>
-          {item.future_edu_jawab === "" ? "No Araz" : "View Jawab"}
-        </div>
-      </div>
-    </div>
-    }
-    {item.current_edu_course !== " " &&
-    <div className='d-flex user-courses-container current'>
-      <div style={{fontWeight:700 , fontSize:"16px" , lineHeight:"19px" , fontFamily:"Inter"}}>Current Education</div>
-      <div>
-        <div className='course-text'>
-          {item.current_edu_course}
-        </div>
-        <div className='course-text'>
-          {item.current_edu_institute}
-        </div>
-        <div className={`jawab-button ${item.current_edu_jawab === "" ? "danger-btn" : "success-btn"}`}>
-          {item.current_edu_jawab === "" ? "No Araz" : "View Jawab"}
-        </div>
-      </div>
-    </div>
-    }
-    {item.last_edu_course !== " " &&
-    <div className='d-flex user-courses-container last'>
-      <div style={{fontWeight:700 , fontSize:"16px" , lineHeight:"19px" , fontFamily:"Inter"}}>Last Education</div>
-      <div>
-        <div className='course-text'>
-          {item.last_edu_course}
-        </div>
-        <div className='course-text'>
-          {item.last_edu_institute}
-        </div>
-        <div className={`jawab-button ${item.last_edu_jawab === "" ? "danger-btn" : "success-btn"}`}>
-          {item.last_edu_jawab === "" ? "No Araz" : "View Jawab"}
-        </div>
-      </div>
-    </div>
-    }
-    </div>
-    </div>
-  </div>
-    ))
-  }
+   <EducationDetail dropoutList={dropoutList} />
 </Col>
 </Row>
 </>
---
+++ src/components/EducationDetail.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
+export default function (props) {
+
+    const {dropoutList} = props;
+    console.log(props)
+
+  return (
+    <>
+    {
+        dropoutList &&  dropoutList.map((item) => (
+       <div className='user-card'>
+         <div className='d-flex'>
+           <div className='d-flex' style={{alignItems:"center" , height:"100%" , width:"42%"}} >
+             <img src={`https://www.talabulilm.com/mumin_images/${item.its_id}.png`} className="user-img"/>
+             <div className='user-details'>
+               <p>{item.its_id}</p>
+               <p>{item.name}</p>
+               <p>{item.gender == 'M' ? 'Male' : 'Female'} {item.age} years</p>
+               <p>{item.email}</p>
+               <p>{item.mobile}</p>
+             </div>
+           </div>
+     
+        <div style={{width:"58%"}} >
+         {item.future_edu_course !== " " &&
+         <div className='d-flex user-courses-container future'>
+           <div style={{fontWeight:700 , fontSize:"16px" , lineHeight:"19px" , fontFamily:"Inter"}}>Future Education</div>
+           <div className='course-container'>
+             <div  >
+             <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{item.future_edu_course}</Tooltip>}>
+             <div className='course-text'>
+               {item.future_edu_course.slice(0,40) +  "...."}
+             </div>
+             </OverlayTrigger>    
+             <div className='course-text'>
+             <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{item.future_edu_course}</Tooltip>}>
+             <div className='course-text'>
+             {item.future_edu_institute.slice(0,40) + "...."}
+             </div>
+             </OverlayTrigger>  
+             </div>
+             </div>
+             <div className={`jawab-button ${item.future_edu_jawab === "" ? "danger-btn" : "success-btn"}`}>
+               {item.future_edu_jawab === "" ? "No Araz" : "View Jawab"}
+             </div>
+           </div>
+         </div>
+         }
+         {item.current_edu_course !== " " &&
+         <div className='d-flex user-courses-container current'>
+           <div style={{fontWeight:700 , fontSize:"16px" , lineHeight:"19px" , fontFamily:"Inter"}}>Current Education</div>
+           <div className='course-container'>
+             <div>
+     
+             <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{item.current_edu_course}</Tooltip>}>
+             <div className='course-text'>
+             {item.current_edu_course.slice(0,40) +  "...."}
+             </div>
+             </OverlayTrigger> 
+             <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{item.current_edu_institute}</Tooltip>}>
+             <div className='course-text'>
+             {item.current_edu_institute.slice(0,40) + "...."}
+             </div>
+             </OverlayTrigger> 
+             </div>
+             <div className={`jawab-button ${item.current_edu_jawab === "" ? "danger-btn" : "success-btn"}`}>
+               {item.current_edu_jawab === "" ? "No Araz" : "View Jawab"}
+             </div>
+           </div>
+         </div>
+         }
+         {item.last_edu_course !== " " &&
+         <div className='d-flex user-courses-container last'>
+           <div style={{fontWeight:700 , fontSize:"16px" , lineHeight:"19px" , fontFamily:"Inter"}}>Last Education</div>
+           <div className='course-container'>
+             <div>
+               
+             <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{item.last_edu_institute}</Tooltip>}>
+             <div className='course-text'>
+             {item.last_edu_institute.slice(0,40) +  "...."}
+             </div>
+             </OverlayTrigger> 
+             <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">{item.last_edu_course}</Tooltip>}>
+             <div className='course-text'>
+             {item.last_edu_course.slice(0,40) +  "...."}
+             </div>
+             </OverlayTrigger> 
+             </div>
+             <div className={`jawab-button ${item.last_edu_jawab === "" ? "danger-btn" : "success-btn"}`}>
+               {item.last_edu_jawab === "" ? "No Araz" : "View Jawab"}
+             </div>
+           </div>
+         </div>
+         }
+         </div>
+         </div>
+       </div>
+         ))
+       }
+  </>
+  )
+
+}
