dba49095dd05aed0f2ed7b745ce50d493beacf8b
mustafasr <74657562+mustafasr@users.noreply.github.com>
2022-06-30 18:53:05 +0530

    merge conflict solved

--- src/App.js
+++ src/App.js
@@ -1,64 +1,6 @@
 import './App.css';
 import { useContext } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { useState } from 'react';
-import useFetch from "./useFetch";
-import EducationDetail from './components/EducationDetail';
-import Sidebar from './components/Sidebar';
-import HashLoader from "react-spinners/HashLoader";
-import logo from "./logotal.png"
-import LinkElement from './components/Sidebar/LinkElement';
-
-export default function App() {
-  const [dropoutList , setDropoutList] = useFetch('https://www.talabulilm.com/api2022/profile/aamilsaheb/dropOutUserList/170');
-  const [EduStatus , setEduStatus] = useState("Drop Outs");
-  let [color, setColor] = useState("#00336D");
-
-  const onLoad = {
-    height: "100vh"
-  }
-
-  return (
-    <>
-      <Navbar style={{backgroundColor:"#002147" , height: "120px" }} >
-        <Container fluid >
-          <div className="d-flex justify-content-between nav-container">
-          <img className="" src={logo} alt='img' />
-          Current Education Status of Ahmedabad Jamaat (Age: 3-27)
-            <div className='d-flex'>
-              <div className="image-header" >Shaikh Mustafa bhai Shaikh Jafar bhai Moaiyadi</div>
-              <img className='image-content' src={`https://www.talabulilm.com/mumin_images/20352890.png`} alt='img' />
-            </div>
-          </div>
-        </Container>
-      </Navbar>
-      <Row style={{backgroundColor:"#E5E5E5" , margin:"0px" }}>
-        <Col xs={3} style={{backgroundColor:"#fff" , marginTop:"20px"}}>
-          <LinkElement />
-        </Col>
-        <Col xs={9} style={{marginTop:"20px"}}>
-          <h3 className='page-title'> <span> Showing results for "{EduStatus}" in Ahmedabad Jamaat </span>
-          <Button   className="button-downlaod">
-          <a  style={{textDecoration:"none" , color:"#ffff" }} href="https://talabulilm.com/profile/csvdownload.php" target = "_blank" > Download full file</a>
-           </Button>
-          </h3>
-
-          {
-            dropoutList && dropoutList.length !== 0 ?  <EducationDetail dropoutList={dropoutList} /> :
-            dropoutList && dropoutList.length  === 0 ? <div className='loader-content'>No data found ....</div> :
-            <div className='loader-content' >
-              <HashLoader color={color} size={78} />
-            </div>
-          }
-        </Col>
-      </Row>
-=======
 import Routes from "./Routes/AppRoutes";
 import UserContext from './Context';
 import { UserProvider } from './Context';
@@ -73,7 +15,6 @@ export default function App() {
     <UserProvider value={user}>
     <Routes/>
     </UserProvider>
->>>>>>> 18abd418391c86d57a7113e773676fb3f2a718af
     </>
   );
 }
