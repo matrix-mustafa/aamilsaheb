b62f45fafb2599277606dc2475192ad4440f3851
mustafasr <74657562+mustafasr@users.noreply.github.com>
2022-07-05 20:45:42 +0530

    import of educationDetail Component

--- src/components/MuzeProfileForm.jsx
+++ src/components/MuzeProfileForm.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import useFetch from "../useFetch";
 import { useEffect } from 'react';
 import Select from 'react-select'
+import EducationDetail from './EducationDetail'
 
 export default function MuzeProfileForm() {
   const [murhala , setMurhala] = useFetch();
