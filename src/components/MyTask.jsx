0978e042aa6bf4dcdfef28e0fcb4306b00e876d4
mustafasr <74657562+mustafasr@users.noreply.github.com>
2022-07-14 14:20:19 +0530

    Form Updates. Not perfectly working

--- src/components/MyTask.jsx
+++ src/components/MyTask.jsx
@@ -1,12 +1,10 @@
-import Accordion from 'react-bootstrap/Accordion';
 import { useAccordionButton } from 'react-bootstrap/AccordionButton';
 import "./AccordionFilter.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCircleNotch , faCheck} from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 import DatePicker from "react-datepicker";
-import Button from 'react-bootstrap/Button';
-
+import { Accordion, Button, Form, Row } from 'react-bootstrap'
 import "react-datepicker/dist/react-datepicker.css";
 
 
@@ -33,71 +31,74 @@ function MyTask(props) {
    const {myTask} = props;
     const [startDate, setStartDate] = useState(new Date());
 
-    const SubIdNull = {
-      border: "1px solid #FFC107"
-    }
-
-    const GetSubId = {
-      border: "1px solid #6E777F"
-    }
-
-    const textColorMuted = {
-      color:"#957B2E"
-    }
-
-    const textColorNotMuted = {
-      color: "#957B2E"
-    }
-
-
   return (
-    <div  className="col-12 px-5 mt-3 " >
+    <div  className="col-12 px-2 mt-3 " >
       {
-        myTask?.map((item) => (
+        myTask?.map((item, idx) => (
 
-          <Accordion >
-          <Accordion.Item eventKey="0" className='mb-3'   style={{ ...(item.sub_id === null ? SubIdNull: GetSubId)}} >
-            <Accordion.Header className='border-bottom border-warning' >
+          <Accordion key={idx}>
+          <Accordion.Item eventKey="0" className={`mb-2 ${item.sub_id === null ? 'border-warning' : 'border-success'}`} >
+            <Accordion.Header className='border-bottom' >
               {
-                item.sub_id === null ? <FontAwesomeIcon color={"#957B2E"} icon={faCircleNotch} /> : <FontAwesomeIcon color={"#6E777F"} icon={faCheck} />
+                item.sub_id === null ? <FontAwesomeIcon color={"#F7D47F"} icon={faCircleNotch} /> : <FontAwesomeIcon color={"#198753"} icon={faCheck} />
               }
-             <div className='mx-2' style={{...(item.sub_id === null ? textColorNotMuted: textColorMuted)}}>{item.task_title}</div> 
+             <div className={`mx-2 ${item.sub_id === null ? 'text-warning' : 'text-success'}`}>{item.task_title}</div>
               </Accordion.Header>
             <Accordion.Body className='p-0' >
-              <div className="text-note p-3" style={{...(item.sub_id === null ? textColorNotMuted: textColorMuted)}}  >
+              <div className="text-muted p-3" >
                 {item.task_description}
               </div>
-              <div  className='d-flex col-12 border-top border-warning '  >
-                  <div className='col-7 p-3'>
-                  <textarea style={{ height:"72px" }} className="form-control" id="its_id_list" required="" name="its_id_list" rows="8" ></textarea>
+              <div className='row border-top' >
+    <Form>
+      <Row>
+      <Form.Group className="p-2 col-12 col-md-6" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="p-2 col-12 col-md-6" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </Row>
+    </Form>
+                  <div className='col-12 col-md-7'>
+                    <textarea style={{ height:"72px" }} className="form-control" id="its_id_list" required="" name="its_id_list" rows="8" ></textarea>
                   </div>
-                  <div className='col-5  py-3'>
-                      <div className='d-flex' >
+                  <div className='col-12 col-md-5'>
+                    <div className='row'>
+                      <div className='col-12 col-md-6' >
                           <div className='mx-2' >Attach Report</div>
-                          <input type="file"  />
+                          <input type="file" />
                       </div>
-                      <div className='d-flex my-2 '>  
-                          <div className='mx-2' >Date of Barnamaj</div> 
-                          <div style={{width:"27%" }} >
+                      <div className='col-12 col-md-6'>
+                        <div className='mx-2' >Date of Barnamaj</div>
+                        <div>
                           <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-                          </div>
+                        </div>
                           <Button className='mx-3' variant="success" >Submit</Button>
+                        </div>
                       </div>
 
-                     
                   </div>
-             
+
               </div>
-      
+
             </Accordion.Body>
-           
+
           </Accordion.Item>
         </Accordion>
 
         ))
       }
 
-   
+
     </div>
   );
 }
